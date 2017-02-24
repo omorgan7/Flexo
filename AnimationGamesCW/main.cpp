2efe5fce84ce93eb1dd648dfe63ffb7f8f5e0cc2
Owen Morgan <o.morgan@bath.ac.uk>
2017-02-24 10:52:04 +0000

    Added parser for simple 2D mesh

--- AnimationGamesCW/main.cpp
+++ AnimationGamesCW/main.cpp
@@ -9,10 +9,14 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <vector>
+
 #include <string>
 
-#include "tiny_obj_loader.h"
+
 #include "shader.hpp"
+#include "objloader.hpp"
 #include "loadGL.hpp"
 
 int main(int argc, const char * argv[]) {
@@ -24,6 +28,13 @@ int main(int argc, const char * argv[]) {
         return EXIT_FAILURE;
     }
     glClearColor(1.0f, 1.0f, 1.0f, 0.0f); //makes the screen blue.
+    // Enable depth test
+    //glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+    
+    // Cull triangles which normal is not towards the camera
+    glEnable(GL_CULL_FACE);
     
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
@@ -35,18 +46,31 @@ int main(int argc, const char * argv[]) {
     auto fragmentPath = ShaderPath + "SimpleFragmentShader.glsl";
     GLuint programID = LoadShaders( vertexPath.c_str(), fragmentPath.c_str());
     
+    std::vector<glm::vec3> vertices;
+    std::vector<unsigned int> vertexIndices;
+//    std::vector<glm::vec2> uvs;
+//    std::vector<glm::vec3> normals;
     
-    static const GLfloat g_vertex_buffer_data[ ] = {
-        0.0f,  0.5f, 1.0f, 0.0f, 0.0f, // Vertex 1: Red
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // Vertex 2: Green
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f  // Vertex 3: Blue
-    };
-    
+    bool res = loadSimpleOBJ("/Users/Owen/Dropbox/grid2.obj", vertices, vertexIndices);
+    std::cout<<res<<"\n";
+
+    for(auto i = vertices.begin(); i !=vertices.end(); i++){
+        i->x *= 0.33f;
+        i->y *= 0.33f;
+        i->z *= 0.33f;
+
+    }
+
+    //vertices
     GLuint vertexbuffer;
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
     
+    GLuint elementbuffer;
+    glGenBuffers(1, &elementbuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,elementbuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,vertexIndices.size()*sizeof(unsigned int), &vertexIndices[0],GL_STATIC_DRAW);
     
     
     do{
@@ -61,17 +85,12 @@ int main(int argc, const char * argv[]) {
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
         GLint posAttrib = glGetAttribLocation(programID, "position");
         glEnableVertexAttribArray(posAttrib);
-        glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,
-                              5*sizeof(float), 0);
-        
-        GLint colAttrib = glGetAttribLocation(programID, "color");
-        glEnableVertexAttribArray(colAttrib);
-        glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE,
-                              5*sizeof(float), (void*)(2*sizeof(float)));
+        glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE,
+                              0, (void*)0);
         
 
         // Draw the triangle !
-        glDrawArrays(GL_LINE_LOOP, 0, 3); // 3 indices starting at 0 -> 1 triangle
+        glDrawElements(GL_LINE_LOOP, (vertices.size()-1)*3, GL_UNSIGNED_INT,0); // 3 indices starting at 0 -> 1 triangle
         
         glDisableVertexAttribArray(0);
         
